77526fd8abd95fa2d1e4f3dd8b51cb2fa2191783
Esperanza <esperanza.aviles@clarity.ai>
2023-02-14 16:31:16 +0100

    add fibonacci numbers generator

---
+++ main/src/main/java/FibonacciNumbers.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class FibonacciNumbers {
+
+  public long generate(String inputNumber) {
+    ArrayList<Long> fibonacciNumbers = new ArrayList<>();
+    fibonacciNumbers.add(0L);
+    fibonacciNumbers.add(1L);
+
+    int input = Integer.parseInt(inputNumber);
+    while (fibonacciNumbers.size() <= input) {
+      Long lastElement = fibonacciNumbers.get(fibonacciNumbers.size() - 1);
+      Long almostLastElement = fibonacciNumbers.get(fibonacciNumbers.size() - 2);
+      fibonacciNumbers.add(lastElement + almostLastElement);
+    }
+
+    System.out.println(fibonacciNumbers.get(fibonacciNumbers.size() - 1));
+
+    return fibonacciNumbers.get(fibonacciNumbers.size() - 1);
+  }
+
+}
---
+++ main/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+  public static void main(String[] args) {
+    FibonacciNumbers fibonacciNumbers = new FibonacciNumbers();
+    fibonacciNumbers.generate("7");
+  }
+
+}
