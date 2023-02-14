283569b561c17c4d0e30faede15ff9834e47a6de
Esperanza <esperanza.aviles@clarity.ai>
2023-02-14 16:46:25 +0100

    create presenter for displaying options in CLI

--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -1,10 +1,12 @@
 import domain.FibonacciNumbers;
+import infrastructure.CliPresenter;
 
 public class Main {
 
   public static void main(String[] args) {
+    CliPresenter cliPresenter = new CliPresenter();
+    cliPresenter.displayOptions();
     FibonacciNumbers fibonacciNumbers = new FibonacciNumbers();
     fibonacciNumbers.generate("7");
   }
-
 }
---
+++ main/src/main/java/infrastructure/CliPresenter.java
@@ -0,0 +1,13 @@
+package infrastructure;
+
+public class CliPresenter {
+
+  public void displayOptions() {
+    System.out.println("Choose one of the following options:");
+    System.out.println("0 - Show created tasks");
+    System.out.println("1 - Create new task");
+    System.out.println("2 - Start a task");
+    System.out.println("3 - See results of completed tasks");
+    System.out.println("4 - Exit program");
+  }
+}
